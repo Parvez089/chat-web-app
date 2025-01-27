a46820ef6d65d2d55c36b357dd8d2830c5482cb4
parvez <mhparvez089@gmail.com>
2025-01-27 21:46:49 +0600

    message routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js"
-
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-dotenv.config()
+
 
 
 const app = express();
-dotenv.config()
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());  // to parse the incomming requests with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 // app.get("/",(req,res)=>{
 //     // root route http://localhost:5000/
